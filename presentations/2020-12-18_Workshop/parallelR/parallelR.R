9e3b91c334c6c16af75df3c038cb6945090a5074
Thomas Girke <tgirke@citrus.ucr.edu>
2020-12-16 19:31:16 -0800

    workshop updates

--- presentations/2020-12-18_Workshop/parallelR/parallelR.R
+++ presentations/2020-12-18_Workshop/parallelR/parallelR.R
@@ -90,3 +90,55 @@ read_tsv("iris.txt") %>% # Import with read_tbv from readr package
 
 ## sbatch script_name.sh
 
+
+## ----working_env, eval=FALSE--------------------------------------------------
+## dir.create("mytestdir")
+## setwd("mytestdir")
+## download.file("https://goo.gl/tLMddb", "slurm.tmpl")
+## download.file("https://goo.gl/5HrYkE", ".batchtools.conf.R")
+
+
+## ----load_pkgs, eval=FALSE----------------------------------------------------
+## library('RenvModule')
+## module('load','slurm') # Loads slurm among other modules
+## 
+## library(batchtools)
+## myFct <- function(x) {
+## 	result <- cbind(iris[x, 1:4,],
+## 	Node=system("hostname", intern=TRUE),
+## 	Rversion=paste(R.Version()[6:7], collapse="."))
+## }
+
+
+## ----submit_jobs, eval=FALSE--------------------------------------------------
+## reg <- makeRegistry(file.dir="myregdir", conf.file=".batchtools.conf.R")
+## Njobs <- 1:4 # Define number of jobs (here 4)
+## ids <- batchMap(fun=myFct, x=Njobs)
+## done <- submitJobs(ids, reg=reg, resources=list(partition="short", walltime=60, ntasks=1, ncpus=1, memory=1024))
+## waitForJobs() # Wait until jobs are completed
+
+
+## ----job_status, eval=FALSE---------------------------------------------------
+## getStatus() # Summarize job status
+## showLog(Njobs[1])
+## # killJobs(Njobs) # # Possible from within R or outside with scancel
+
+
+## ----assemble_results, eval=FALSE---------------------------------------------
+## readRDS("myregdir/results/1.rds") # reads from rds file first result chunk
+## loadResult(1)
+## lapply(Njobs, loadResult)
+## reduceResults(rbind) # Assemble result chunks in single data.frame
+## do.call("rbind", lapply(Njobs, loadResult))
+
+
+## ----clear_delete_registry, eval=FALSE----------------------------------------
+## clearRegistry() # Clear registry in R session
+## removeRegistry(wait=0, reg=reg) # Delete registry directory
+## # unlink("myregdir", recursive=TRUE) # Same as previous line
+
+
+## ----load_registry, eval=FALSE------------------------------------------------
+## from_file <- loadRegistry("myregdir", conf.file=".batchtools.conf.R")
+## reduceResults(rbind)
+
